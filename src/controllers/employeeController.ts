4f6f61a3ad3120f55479f5fa61fc5041bc9601c5
Felipe Mendes Neves <felipemen74@gmail.com>
2025-07-21 20:51:53 -0300

    feat: adiciona o restante do crud referente a employee

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -14,6 +14,61 @@ async function createEmployee(specialization: string, userId: string) {
     }
 }
 
+async function getEmployee(id: number) {
+    try {
+        const employee = await prisma.employee.findUnique({
+            where: {
+                id: id
+            }
+        });
+        return employee;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function getEmployees() {
+    try {
+        const employees = await prisma.employee.findMany();
+        return employees;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function updateEmployee(id: number, specialization: string) {
+    try {
+        const employee = await prisma.employee.update({
+            where: {
+                id: id
+            },
+            data: {
+                specialization: specialization
+            }
+        });
+        return employee;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function deleteEmployee(id: number) {
+    try {
+        const employee = await prisma.employee.delete({
+            where: {
+                id: id
+            }
+        });
+        return employee;
+    } catch (error) {
+        throw error;
+    }
+}
+
 export default {
-    createEmployee
+    createEmployee,
+    getEmployee,
+    getEmployees,
+    updateEmployee,
+    deleteEmployee
 }
--- src/routes/employeeRoutes.ts
+++ src/routes/employeeRoutes.ts
@@ -15,4 +15,47 @@ router.post("/employee", auth, async (req: any, res: any) => {
     }
 });
 
+router.get("/employee/:id", auth, async (req: any, res: any) => {
+    const {id} = req.params;
+    if(!id) return res.status(401).json({ error: "Id inválido!" });
+    try {
+        const employee = await employeeController.getEmployee(parseInt(id));
+        return res.status(200).json({ employee: employee });
+    } catch (error) {
+        return res.status(500).json({ error: "Erro ao buscar funcionário!" });
+    }
+});
+
+router.get("/employees", auth, async (req: any, res: any) => {
+    try {
+        const employees = await employeeController.getEmployees();
+        return res.status(200).json({ employees: employees });
+    } catch (error) {
+        return res.status(500).json({ error: "Erro ao buscar funcionários!" });
+    }
+});
+
+router.patch("/employee/:id", auth, async (req: any, res: any) => {
+    const { id } = req.params;
+    const { specialization } = req.body;
+    if(!id || !specialization) return res.status(400).json({ error: "Todos os campos devem ser preenchidos!" });
+    try {
+        const employee = await employeeController.updateEmployee(parseInt(id), specialization);
+        return res.status(200).json({ message: "Funcionario atualizado com sucesso!", "employee": employee });
+    } catch (error) {
+        return res.status(500).json({ error: "Erro ao atualizar funcionário!" });
+    }
+});
+
+router.delete("/employee/:id", auth, async (req: any, res: any) => {
+    const { id } = req.params;
+    if(!id) return res.status(400).json({ error: "Id inválido!" });
+    try {
+        await employeeController.deleteEmployee(parseInt(id));
+        return res.status(200).json({ message: "Funcionario excluido com sucesso!" });
+    } catch (error) {
+        return res.status(500).json({ error: "Erro ao excluir funcionário!" });
+    }
+});
+
 export default router;
